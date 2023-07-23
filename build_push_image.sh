72c633a919a43786aa7480da4a237a1295e32aee
rzfd <rizkifadillah783@gmail.com>
2023-07-23 23:04:55 +0700

    ada

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,4 +1,20 @@
-USERNAME="rzfd"
-REPO_NAME="a433-microservices"
+#!/bin/bash
+
+# Nama image dan tag yang akan digunakan
 IMAGE_NAME="item-app"
 IMAGE_TAG="v1"
+
+# Step 1: Membuat Docker image dari Dockerfile
+docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
+
+# Step 2: Melihat daftar image di lokal
+docker images
+
+# Step 3: Mengubah nama image agar sesuai dengan format Docker Hub
+docker tag "$IMAGE_NAME:$IMAGE_TAG" "dockerhubusername/$IMAGE_NAME:$IMAGE_TAG"
+
+# Step 4: Login ke Docker Hub 
+docker login
+
+# Step 5: Mengunggah image ke Docker Hub (atau GitHub Packages jika diterapkan)
+docker push "tarjo/$IMAGE_NAME:$IMAGE_TAG"
